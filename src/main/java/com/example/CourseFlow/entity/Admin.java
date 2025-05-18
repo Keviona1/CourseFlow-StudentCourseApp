ee9058d64b13c2cffe22f1deb21a0ee8a8a1feb2
Keviona1 <kevionasaliasi@gmail.com>
2025-05-18 11:33:36 +0200

    Admin Implementation

--- src/main/java/com/example/CourseFlow/entity/Admin.java
+++ src/main/java/com/example/CourseFlow/entity/Admin.java
@@ -1,4 +1,14 @@
 package com.example.CourseFlow.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
 }

8e093568a65446d1cfd3e9418a0d111d30305dda
kev <kevionasaliasi14@hotmail.com>
2025-05-18 15:05:07 +0200

    Course update

--- src/main/java/com/example/CourseFlow/entity/Course.java
+++ src/main/java/com/example/CourseFlow/entity/Course.java
@@ -1,11 +1,85 @@
 package com.example.CourseFlow.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
+@Table(name = "courses")
 public class Course {
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Enrollment> enrollments;
+
+
+    @Column(name = "start_time")
+    private LocalDateTime start_time;
+
+    @Column(name = "end_time")
+    private LocalDateTime end_time;
+
+
+    public Course() {}
+
+    public Course(String name, String description, List<Enrollment> enrollments, LocalDateTime start_time, LocalDateTime end_time) {
+        this.name = name;
+        this.description = description;
+        this.enrollments = enrollments;
+        this.start_time = start_time;
+        this.end_time = end_time;
+    }
+
+    // Getter and setter methods
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public List<Enrollment> getEnrollments() {
+        return enrollments;
+    }
+    public void setEnrollments(List<Enrollment> enrollments) {
+        this.enrollments = enrollments;
+    }
+
+    public LocalDateTime getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(LocalDateTime start_time) {
+        this.start_time = start_time;
+    }
+
+    public LocalDateTime getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(LocalDateTime end_time) {
+        this.end_time = end_time;
+    }
 }

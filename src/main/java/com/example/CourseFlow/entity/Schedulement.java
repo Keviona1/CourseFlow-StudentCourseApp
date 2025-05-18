b8e926dff0ebfed77144a27d42aeb518722f36f2
Redon Allushi <redonallushi1998@gmail.com>
2025-05-18 13:51:31 +0200

    admin implementation 1

---
+++ src/main/java/com/example/CourseFlow/entity/Schedulement.java
@@ -0,0 +1,11 @@
+package com.example.CourseFlow.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Schedulement {
+
+    @Id
+    private Long id;
+}

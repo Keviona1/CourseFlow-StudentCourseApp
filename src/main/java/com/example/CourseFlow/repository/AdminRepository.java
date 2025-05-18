cae3acc5de512e92a9e58fdde82de39d03be33c7
Redon Allushi <redonallushi1998@gmail.com>
2025-05-18 13:35:26 +0200

    admin implementation

---
+++ src/main/java/com/example/CourseFlow/repository/AdminRepository.java
@@ -0,0 +1,8 @@
+package com.example.CourseFlow.repository;
+
+import com.example.CourseFlow.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Admin findByUsername(String username);
+}

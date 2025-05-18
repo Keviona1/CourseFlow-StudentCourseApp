636ba7783fdb36057825a0e3974fc679b16b55e3
kev <kevionasaliasi14@hotmail.com>
2025-05-18 10:25:29 +0200

    Course Implementation

---
+++ src/main/java/com/example/CourseFlow/service/CourseService.java
@@ -0,0 +1,34 @@
+package com.example.CourseFlow.service;
+
+import com.example.CourseFlow.entity.Course;
+import com.example.CourseFlow.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CourseService {
+    @Autowired
+    private CourseRepository courseRepository;
+
+
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+
+    public Course getCourseById(Long id) {
+        return courseRepository.findById(id).orElse(null);
+    }
+
+
+    public void saveCourse(Course course) {
+        courseRepository.save(course);
+    }
+
+
+    public void deleteCourse(Long id) {
+        courseRepository.deleteById(id);
+    }
+}

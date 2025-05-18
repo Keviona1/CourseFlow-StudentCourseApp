7336e26ad351868f83c5c5f37efca72f8ac5bbe1
kev <kevionasaliasi14@hotmail.com>
2025-05-18 10:26:05 +0200

    Course Implementation

--- src/main/java/com/example/CourseFlow/entity/Course.java
+++ src/main/java/com/example/CourseFlow/entity/Course.java
@@ -1,4 +1,58 @@
 package com.example.CourseFlow.entity;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "courses")
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Enrollment> enrollments;
+
+
+
+    public Course() {}
+
+    public Course(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public List<Enrollment> getEnrollments() {
+        return enrollments;
+    }
+    public void setEnrollments(List<Enrollment> enrollments) {
+        this.enrollments = enrollments;
+    }
 }
